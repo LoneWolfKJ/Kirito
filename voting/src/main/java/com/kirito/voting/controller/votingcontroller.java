c17978658e9e5b818016fffca3a43478b6d2d612
Kanjal <kanjaldalal1000@gmail.com>
2019-07-26 14:20:16 +0530

    controller

--- voting/src/main/java/com/kirito/voting/controller/votingcontroller.java
+++ voting/src/main/java/com/kirito/voting/controller/votingcontroller.java
@@ -1,4 +1,35 @@
 package com.kirito.voting.controller;
 
+import com.kirito.voting.dto.getVote;
+import com.kirito.voting.dto.voteresponse;
+import com.kirito.voting.services.votingService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@RequestMapping(votingcontroller.VOTING_API_ENDPOINT)
 public class votingcontroller {
+    public static final String VOTING_API_ENDPOINT = "/voting/v1";
+    public static final String VOTING_API = "/vote";
+
+
+    @Autowired
+    private votingService votingservice;
+
+    @PostMapping(VOTING_API)
+    public ResponseEntity<voteresponse> vote(@RequestBody getVote getvote){
+        try {
+            voteresponse response = votingservice.givevote(getvote);
+            return ResponseEntity.ok().body(response);
+        } catch (Exception x) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
 }
---
+++ voting/src/main/java/com/kirito/voting/services/votingServiceImpl.java
@@ -0,0 +1,13 @@
+package com.kirito.voting.services;
+
+import com.kirito.voting.dto.getVote;
+import com.kirito.voting.dto.voteresponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class votingServiceImpl implements votingService{
+    @Override
+    public voteresponse givevote(getVote getvote) {
+        return null;
+    }
+}
