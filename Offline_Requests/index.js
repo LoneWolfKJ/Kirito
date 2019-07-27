c941f25d7c06101e3470727be9afa8b33173704a
shahayush457 <shahayush457@gmail.com>
2019-07-27 18:41:17 +0530

    modified index.js

--- Offline_Requests/index.js
+++ Offline_Requests/index.js
@@ -1,5 +1,5 @@
 import * as fuck from './app.js';
-import * as axios from 'axios';
+import axios from 'axios';
 const voter_id = document.getElementById('vote').value;
 const password = document.getElementById('pass').value;
 const vote_to = document.getElementById('delegate').value;
@@ -7,22 +7,20 @@ const postData = {
     voter_id: "voter_id",
     password: "password",
     vote_to: "vote_to"
-  };
+};
   
-  let axiosConfig = {
-    headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
-        "Access-Control-Allow-Origin": "*",
-    }
-  };
+//   let axiosConfig = {
+//     headers: {
+//         'Content-Type': 'application/json;charset=UTF-8',
+//         "Access-Control-Allow-Origin": "*",
+//     }
+//   };
   
-  axios.post('./voting/v1/vote', postData, axiosConfig)
+  axios.post('./voting/v1/vote', postData)
   .then((res) => {
     console.log("RESPONSE RECEIVED: ", res);
   })
   .catch((err) => {
     console.log("AXIOS ERROR: ", err);
-    // const off = new offline();
-    // off.send();
     fuck.send();
   });
