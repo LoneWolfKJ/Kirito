37a756b5fde5760dbddbf80c5763566bdb5fc128
shahayush457 <shahayush457@gmail.com>
2019-07-28 10:01:44 +0530

    added favicon

--- Offline_Requests/index.js
+++ Offline_Requests/index.js
@@ -1,5 +1,8 @@
 import * as fuck from './app.js';
-import axios from 'axios';
+//import axios from 'axios';
+// const express = require('express');
+// const app = express();
+
 const voter_id = document.getElementById('vote').value;
 const password = document.getElementById('pass').value;
 const vote_to = document.getElementById('delegate').value;
@@ -8,7 +11,8 @@ const postData = {
     password: "password",
     vote_to: "vote_to"
 };
-  
+const Url = '/voting/v1/vote';
+const port = 5000;
 //   let axiosConfig = {
 //     headers: {
 //         'Content-Type': 'application/json;charset=UTF-8',
@@ -16,11 +20,23 @@ const postData = {
 //     }
 //   };
   
-  axios.post('./voting/v1/vote', postData)
-  .then((res) => {
-    console.log("RESPONSE RECEIVED: ", res);
-  })
-  .catch((err) => {
-    console.log("AXIOS ERROR: ", err);
-    fuck.send();
-  });
+//   axios.post('./voting/v1/vote', postData)
+//   .then((res) => {
+//     console.log("RESPONSE RECEIVED: ", res);
+//   })
+//   .catch((err) => {
+//     console.log("AXIOS ERROR: ", err);
+//     fuck.send();
+//   });
+
+$('.btn').click(function(){
+    $.post(Url, postData, function(status){
+        console.log('status is ${status');
+        if(status != 200)
+        fuck.send();
+    });
+});
+
+// app.listen(port, function(){
+//     console.log("Server running at port :" +port);
+// });
