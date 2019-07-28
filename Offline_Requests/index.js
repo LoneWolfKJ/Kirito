2dfdf4d31ddb47a19ce077843a81559798be40d0
shahayush457 <shahayush457@gmail.com>
2019-07-28 14:42:09 +0530

    function added

--- Offline_Requests/index.js
+++ Offline_Requests/index.js
@@ -1,18 +1,22 @@
-import * as fuck from './app.js';
+// import * as fuck from 'app.js';
 //import axios from 'axios';
-// const express = require('express');
-// const app = express();
+//var fuck = require('app.js');
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-const voter_id = document.getElementById('vote').value;
-const password = document.getElementById('pass').value;
-const vote_to = document.getElementById('delegate').value;
+app.use(cors());
+
+// var voter_id = ;
+// var password = ;
+// var vote_to = ;
 const postData = {
-    voter_id: "voter_id",
-    password: "password",
-    vote_to: "vote_to"
+    voter_id: "document.getElementById('vote').value",
+    password: "document.getElementById('pass').value",
+    vote_to: "document.getElementById('delegate').value"
 };
 const Url = '/voting/v1/vote';
-const port = 5000;
+
 //   let axiosConfig = {
 //     headers: {
 //         'Content-Type': 'application/json;charset=UTF-8',
@@ -29,14 +33,58 @@ const port = 5000;
 //     fuck.send();
 //   });
 
+function send(){
+    event.respondWith(
+                                            // Try to get the response from the network
+        fetch(event.request.clone()).catch(function() {	
+        self.clients.match(thisClient).then(function(client) {
+                client.postMessage({
+                message: "Post unsuccessful.",
+             alert: alert                     // A string we instantiated earlier
+                });
+        });                             	// Respond with the page that the request originated from
+        return caches.match(event.request.clone().referrer);
+        })
+    );
+    
+    
+    navigator.serviceWorker.addEventListener('message', function(event) {
+        alert(event.data.alert);
+        store();
+    });
+    
+    function store() {
+        var newPost = "";                                    // Inputted values     
+        $("input").each(function() {                         // Iterate through the inputs
+            newPost += $(this).val() + ",";
+        });                                                  // Get rid of the last comma
+        newPost = newPost.substr(0, newPost.length - 1);     // Store the data
+        localStorage.setItem('newPost', newPost);
+    }
+
+    
+    // Call the function whenever the page is refreshed
+    $(function () {
+        if (localStorage.getItem('newPost')) {	                        // Set keys and values for voting for your favorite article
+            var names = ['voterid', 'password', 'c1'];
+            var values = localStorage.getItem('newPost').split(',');	
+        var form = $("#offlinePostForm"); 	                            // Form to append inputs to
+        for (var i = 0; i < names.length; i++) {                        // Create and append inputs
+            var input = '<input type="hidden" name="' 
+            + names[i] + '" value="' 
+            + values[i] + '">';
+            $(input).appendTo(form);
+        }	                                   // Clear newPost from localStorage and submit the form
+        localStorage.removeItem('newPost');
+        form.submit();
+        }
+    });
+    };
+
 $('.btn').click(function(){
     $.post(Url, postData, function(status){
         console.log('status is ${status');
         if(status != 200)
-        fuck.send();
+        send();
     });
 });
-
-// app.listen(port, function(){
-//     console.log("Server running at port :" +port);
-// });
