9203983a243bdba7a5c2df656bfa67da484dd682
root <kanjaldalal1000@gmail.com>
2019-07-28 15:41:17 +0530

    deleted voting from branch

--- voting/src/main/java/com/kirito/voting/services/votingService.java
+++
@@ -1,8 +0,0 @@
-package com.kirito.voting.services;
-
-import com.kirito.voting.dto.getVote;
-import com.kirito.voting.dto.voteresponse;
-
-public interface votingService {
-    public voteresponse givevote(getVote getvote);
-}
